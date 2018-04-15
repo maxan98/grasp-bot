b2fc330047a902f63b907b6adcc0d79051ac59ce
Max Sklyarov <maxan9888@gmail.com>
2018-04-15 12:10:06 +0300

    attachment

--- vkapi.py
+++ vkapi.py
@@ -8,6 +8,6 @@ def send_message(user_id, token, message, attachment=""):
 
 def get_wall_picture():
 
-    photo = api.photos.get(owner_id=str(164325230), album_id='0', count=0, offset=0)['items'][0]['id']
-    attachment = 'photo' + str(164325230) + '_' + str(photo)
+    photo = api.photos.get(owner_id=str(-164325230), album_id='252800525', count=0, offset=0)['items'][0]['id']
+    attachment = 'photo' + str(-164325230) + '_' + str(photo)
     return attachment
