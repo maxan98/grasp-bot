d4331cd2807857a7ebb2fff1f0efd04cbb5d951a
Max Sklyarov <maxan9888@gmail.com>
2018-04-15 17:04:51 +0300

    first db integr

---
+++ litedb.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class Database():
+
+	
+	def __init__(self):
+		self.con = sqlite3.connect('users.db')
+		self.cur = self.con.cursor()
+	def execread(self,query):
+		self.cur.execute(query)
+		result = cur.fetchall()
+		return result
+	def execwrite(self,query):
+		self.cur.execute(query)
+		self.con.commit()
+
+	def close(self):
+		self.con.close()
+
+
--- messageHandler.py
+++ messageHandler.py
@@ -2,6 +2,7 @@ import vkapi
 import os
 import importlib
 from command_system import command_list
+from litedb import *
 def load_modules():
    # путь от рабочей директории, ее можно изменить в настройках приложения
    files = os.listdir("commands")
@@ -15,11 +16,14 @@ def get_answer(body):
     attachment = ''
     for c in command_list:
         if body in c.keys:
-            message, attachment = c.process()
-    return message, attachment
+            message, attachment, pending = c.process()
+    return message, attachment, pending
 
 def create_answer(data, token):
    load_modules()
    user_id = data['user_id']
-   message, attachment = get_answer(data['body'].lower())
+   message, attachment, pending = get_answer(data['body'].lower())
    vkapi.send_message(user_id, token, message, attachment)
+   db = Database()
+   db.execwrite("insert into users values (NULL,%s,'RandUser',NULL,'user','%s') "%(user_id,pending))
+   sb.close()
