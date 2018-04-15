3d4f524e1eabfaa26b5cb404b2d835c7e98fe742
Max Sklyarov <maxan9888@gmail.com>
2018-04-15 11:28:04 +0300

    minor info fixes

--- commands/info.py
+++ commands/info.py
@@ -1,8 +1,9 @@
 import command_system
 
 def info():
-    message = 'Привет, друг, я - новый бот SUAI. Пока я только учусь, но скоро я смогу помогать тебе не опаздывать на пары!'
+    message = 'Вот то, что я умею на данный момент. Всяко лучше, чем быть одноглазым и с палкой в ноге :)'
     st = [d.description for d in command_system.command_list]
+    desc = '\n'.join(st)
     return message, ""
 info_command = command_system.Command()
 info_command.keys = ['info','help','помощь','инфа','информация','что умеешь']
