6f30f291987ad2e9743aa568f96ab14dae02b018
Max Sklyarov <maxan9888@gmail.com>
2018-04-15 23:06:07 +0300

    a

--- commands/info.py
+++ commands/info.py
@@ -5,7 +5,7 @@ def info():
     st = [d.description +'['+' '.join(d.keys)+']' for d in command_system.command_list]
     desc = '\n'.join(st)
     attachment = vkapi.get_wall_picture()
-    return message + '\n'+desc, attachment, None
+    return message + '\n'+desc, "", None
 info_command = command_system.Command()
 info_command.keys = ['info','help','помощь','инфа','информация','что умеешь']
 info_command.description = 'Расскажу о том, что я умею'
