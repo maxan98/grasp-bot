bd5c3d2acc801a0949ae940449619dcb38d7e4a9
Max Sklyarov <maxan9888@gmail.com>
2018-04-16 08:41:13 +0300

    whole timetable

--- commands/rasp.py
+++ commands/rasp.py
@@ -5,12 +5,14 @@ def rasp(data):
     db = Database()
     answer = db.execread("SELECT gr FROM users WHERE vkid = %s"%data['user_id'])
     db.close()
-    child=sp.Popen(['grasp','-f','%s'%(answer[4:-4])],stdout=sp.PIPE) 
-    s=' '
     res = '' 
-    while s: 
-        s=child.stdout.readline() 
-        res +=s.decode('utf-8')
+    if len(answer)>0:
+        child=sp.Popen(['grasp','-f','%s'%(answer[0])],stdout=sp.PIPE) 
+        s=' '
+        
+        while s: 
+            s=child.stdout.readline() 
+            res +=s.decode('utf-8')
     message = res+' \nЕсли тут пусто - вероятно ты еще не задал номер своей группы(/группа xxxx)'
     return message, "", None
 rasp_command = command_system.Command()
