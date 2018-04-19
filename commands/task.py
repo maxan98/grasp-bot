573157dfa0fc21f19a630d3f88c306df0255647b
Max Sklyarov <maxan9888@gmail.com>
2018-04-19 13:53:36 +0300

    notifications

--- commands/task.py
+++ commands/task.py
@@ -5,7 +5,7 @@ def task(data):
     pending = data['body']
     message = 'Напоминание на завтра. Формат [напоминание 12 00 Сообщение]\n Если до тебя вдруг чисто случайно доперло, что ты написал все не правильно - исправь ошибку и напиши еще раз'
     db = Database()
-    db.execwrite("UPDATE users SET pending = '%s' WHERE CustomerID = %s"%(pending, data['user_id']))
+    db.execwrite("UPDATE users SET pending = '%s' WHERE vkid = %s"%(pending, data['user_id']))
     db.close()
     return message, "", None
 task_command = command_system.Command()
