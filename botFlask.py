ad4dd359928a49aed02f35f92d8a0b775f02bd1b
Max Sklyarov <maxan9888@gmail.com>
2018-04-19 20:06:36 +0300

    manual admin message send

--- botFlask.py
+++ botFlask.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, json
 from settings import token, confirmation_token
 import messageHandler
+import vkapi
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,3 +27,16 @@ def processing():
          messageHandler.create_answer(data['object'], token)
          # Сообщение о том, что обработка прошла успешно
          return 'ok'
+
+@app.route('/suckdick', methods=['POST'])
+def processing():
+    data = json.loads(request.data)
+    i = data['vkid']
+    messa = data['messa']
+    secret = data['secret']
+    if secret == 'suckmydick'
+        vkapi.send_message(i, token, messa, '')
+        return 'ok'
+    else:
+        return 'not vk'
+
